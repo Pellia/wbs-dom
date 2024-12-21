3668d2e800bc702d88e1f0c5b90c3992ee3c694d
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2024-12-21 16:29:40 +0100

    update ex4

--- ex4/script.js
+++ ex4/script.js
@@ -1,22 +1,38 @@
+// Get Main Elements
 const btn = document.querySelector("button");
 const output = document.getElementById("output");
+const inputName = document.getElementById("name");
+const inputEmail = document.getElementById("email");
+const inputMessage = document.getElementById("message");
 
-btn.addEventListener("click", () => {
+// Butten Event
+btn.addEventListener("click", (event) => {
+    event.preventDefault();
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const message = document.getElementById("message").value;
-
+    const inputArr = [name, email, message];
     const inputList = document.createElement("ul");
-    const nameItem = document.createElement("li");
-    const emailItem = document.createElement("li");
-    const messageItem = document.createElement("li");
 
-    if (name !== "" || email !== "" || message !== "") {
-        nameItem.textContent = name;
-        inputList.appendChild(nameItem);
-        output.appendchild(inputList);
-        // alert();
+    // Check for Input Data
+    if (name !== "" && email !== "" && message !== "") {
+        inputArr.forEach((input) => {
+            const item = document.createElement("li");
+            item.textContent = input;
+            item.classList.add("p-4", "mb-4", "text-md", "font-bold", "text-green-800", "rounded-lg", "bg-green-50");
+            inputList.appendChild(item);
+        });
+        output.appendChild(inputList);
     } else {
-        alert();
+        const errorItem = document.createElement("li");
+        errorItem.textContent = "Error";
+        errorItem.classList.add("p-4", "mb-4", "text-md", "font-bold", "text-red-800", "rounded-lg", "bg-red-50");
+        output.appendChild(inputList);
+        inputList.appendChild(errorItem);
     }
+
+    // Clear Input Fields
+    inputName.value = "";
+    inputEmail.value = "";
+    inputMessage.value = "";
 });
