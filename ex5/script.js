7db0840fa541baed6b0f52a0557512d374e91b33
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-01-07 12:14:43 +0100

    update ex5

--- ex5/script.js
+++ ex5/script.js
@@ -1,18 +1,60 @@
+// Elements
+const userScore = document.getElementById("user-score");
+const computerScore = document.getElementById("computer-score");
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorBtn = document.getElementById("scissors");
+const result = document.getElementById("result");
+const playBtn = document.getElementById("play-button");
+let userMove = null;
+
+// Functions
 function getComputerMove() {
-    const move = ["rock", "paper", "scissor"];
+    const move = ["Rock", "Paper", "Scissor"];
     return move[Math.floor(Math.random() * 3)];
 }
 
 function compareMoves(userMove, computerMove) {
     if (userMove === computerMove) {
-        return "It's a tie!";
-    } else if (userMove === "rock" && computerMove === "scissors") {
-        return "You win!";
-    } else if (userMove === "paper" && computerMove === "rock") {
-        return "You win!";
-    } else if (userMove === "scissors" && computerMove === "paper") {
-        return "You win!";
+        result.textContent = "It's a tie!";
+    } else if (userMove === "Rock" && computerMove === "Scissor") {
+        result.textContent = "You win!";
+        userScore.textContent++;
+    } else if (userMove === "Paper" && computerMove === "Rock") {
+        result.textContent = "You win!";
+        userScore.textContent++;
+    } else if (userMove === "Scissor" && computerMove === "Paper") {
+        result.textContent = "You win!";
+        userScore.textContent++;
     } else {
-        return "Computer wins!";
+        result.textContent = "Computer wins!";
+        computerScore.textContent++;
     }
 }
+
+rockBtn.addEventListener("click", () => {
+    rockBtn.classList.add("border-2", "border-black");
+    paperBtn.classList.remove("border-2", "border-black");
+    scissorBtn.classList.remove("border-2", "border-black");
+    userMove = "Rock";
+});
+
+paperBtn.addEventListener("click", () => {
+    rockBtn.classList.remove("border-2", "border-black");
+    paperBtn.classList.add("border-2", "border-black");
+    scissorBtn.classList.remove("border-2", "border-black");
+    userMove = "Paper";
+});
+
+scissorBtn.addEventListener("click", () => {
+    rockBtn.classList.remove("border-2", "border-black");
+    paperBtn.classList.remove("border-2", "border-black");
+    scissorBtn.classList.add("border-2", "border-black");
+    userMove = "Scissor";
+});
+
+playBtn.addEventListener("click", () => {
+    const computerMove = getComputerMove();
+    compareMoves(userMove, computerMove);
+    // console.log();
+});
